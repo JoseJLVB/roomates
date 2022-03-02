6d2b6061f1f18cebf7c70538b1c5597b9eca0b3d
Jose Luis Valcarcel Bravo <josevalcarcel@entrearaucarias.com>
2022-03-01 21:44:37 -0300

    creadoas las funciones de gasto

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 const express = require('express');
 const app = express();
-app.use(express.static('static'));
 const axios = require('axios');
 const uuid = require('uuid');
 const fs = require('fs').promises;
 
+
+app.use(express.static('static'));
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true }) );
 
-async function newRoomate(){
+
+
+app.get('/gastos', async (req, res) => {
+    let db = await fs.readFile('db.json', 'utf-8')
+    db = JSON.parse(database)
+    let gastos = db.gastos
+    res.send({gastos})
+});
+
+app.get('/roommates', async (req, res) => {   
+    let db = await fs.readFile('db.json', 'utf-8')
+    let roommates = db.roommates
+    res.send({roommates})
+});
+
+app.post('/roommates', async(req, res)=>{
+    
     const roomateData = await axios.get('https://randomuser.me/api')
     const randomUser = roomateData.data.results[0]
     const user = {
         id : uuid.v4().slice(30),
         nombre: randomUser.name.first + ' ' + randomUser.name.last,
     }
-    return user
-}
-
-app.post('/roommates', async(req, res)=>{
     
-    const roommate = await newRoomate()
-    const db = await fs.readFile('db.json', 'utf-8')
+    let db = await fs.readFile('db.json', 'utf-8')
     db = JSON.parse(db)
     db.roommates.push(randomUser)
     await fs.writeFile('db.json', JSON.stringify(db), 'utf-8')
-    res.json(db)
+    res.send({todo: 'ok'})
 });
+
+
 
 app.post('/gasto', async (req, res)=>{
     let body;
@@ -51,17 +65,46 @@ app.post('/gasto', async (req, res)=>{
         res.send({todo: 'OK'});
     });
 });
-app.get('/roommates', (req, res) => {   
-    let db = require('./db.json')
-    let roommates = db.roommates
-    res.json({roommates})
+
+
+
+
+app.put('/gasto', async (req, res)=> {
+    const id = req.query.id;
+    let body;
+    req.on('data', (payload) => {
+        body = JSON.parse(payload);
+    });
+    req.on('end', async () => {
+        let db = await fs.readFile('db.json', 'utf-8')
+        db = JSON.parse(db)
+        let gasto = db.gastos.find(g=>g.id === id)
+        let gastopast = gasto.monto
+
+        gasto.roommate = body.roommate
+        gasto.monto = body.monto
+        gasto.descripcion = body.descripcion
+
+        let roommate = db.roommates.find(r => r.nombre == body.roommate);
+        let roomatepast = roommate.debe
+
+        roommate.debe = roommate.debe + (gasto.monto - gastopast)
+
+        await fs.writeFile('db.json', JSON.stringify(db), 'utf-8') 
+        res.send({todo:'ok'})
+    })
 })
 
-app.get('/gastos', (req, res) => {
-    let db = require('./db.json')
-    let gastos = db.gastos
-    res.json({gastos})
+app.delete('/gasto', async(req, res) => {
+    const id = req.query.id;
+    let db = await fs.readFile ('db.json', 'utf-8');
+    db = JSON.parse(db);
+    const deletegastos = db.gastos.filter(x => x.id !== id);
+    db.gastos = deletegastos
+    await fs.writeFile('db.json', JSON.stringify(db), 'utf-8');
+    res.send(db);
 })
+
 
 app.listen(3000, function(){
     console.log('Running server on  port 3000')
